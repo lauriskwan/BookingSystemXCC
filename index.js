0f5ef37012eae9d24e41de74bd5f1630f9005eac
KennyLau2022 <cwlau1049@gmail.com>
2023-02-09 19:10:52 +0800

    Add Handlerbars and router

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { engine } = require("express-handlebars");
 
 // Basic server configuration
 const app = express();
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
@@ -13,9 +13,52 @@ const port = 8080;
 
 //
 app.get("/", (req, res) => {
-    res.render("studentLogin")
+  res.render("homepage", { layout: "loginpage" });
+});
+app.get("/student_login", (req, res) => {
+  res.render("student/studentLogin", { layout: "loginpage" });
+});
+app.get("/instructor_login", (req, res) => {
+  res.render("instructor/instructorLogin", { layout: "loginpage" });
+});
+app.get("/register", (req, res) => {
+  res.render("studentReg", { layout: "loginpage" });
+});
+// Student Page Router
+app.get("/student/course_list", (req, res) => {
+  res.render("student/courseList");
+});
+app.get("/student/course_detail", (req, res) => {
+  res.render("student/courseDetail");
+});
+app.get("/student/my_course", (req, res) => {
+  res.render("student/myCourse");
+});
+app.get("/student/profile", (req, res) => {
+  res.render("profile");
+});
+app.get("/logout", (req, res) => {
+  res.redirect("/"); //return to homepage
+  //res.logOut((err)=>{...})
+});
+// Instructor Page Router
+app.get("/instructor/add_course", (req, res) => {
+  res.render("instructor/addCourse", { layout: "main_instructor" });
+});
+app.get("/instructor/manage_course", (req, res) => {
+  res.render("instructor/manageCourse", { layout: "main_instructor" });
+});
+app.get("/instructor/course_detail", (req, res) => {
+  res.render("instructor/courseDetail", { layout: "main_instructor" });
+});
+app.get("/instructor/profile", (req, res) => {
+  res.render("profile", { layout: "main_instructor" });
+});
+app.get("/logout", (req, res) => {
+  res.redirect("/"); //return to homepage
+  //res.logOut((err)=>{...})
 });
 
 app.listen(port, () => {
-    console.log(`Running at port ${port}`);
+  console.log(`Running at http://localhost:${port}`);
 });
